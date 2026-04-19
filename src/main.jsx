bd0f7584d5befdcd7a82fe0c6c9431875ed463a2
RedroJarr <electrobrick@hotmail.com>
2026-04-19 16:01:30 +0100

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import GalaxyWiki from './GalaxyWiki.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <GalaxyWiki />
+  </React.StrictMode>
+)
